bcbd3162f5b77fca12d5005ab00c6daf91716ad9
jesusDev96 <chechujor@hotmail.com>
2026-03-19 12:05:01 -0300

    variables, concatenacion, prompt. Semana 1

---
+++ semana1-Introduccion/js/main.js
@@ -0,0 +1,19 @@
+// let materia = "javascript"
+// let comision = 89595
+// let flex = true
+
+// console.log("Materia: " + materia)
+// console.log("Comision N°" + comision)
+// console.log("Materia: " + materia + " Comision N°" + comision)
+// alert("Materia: " + materia + " Comision N°" + comision)
+
+// materia = prompt("Ingrese la materia")
+// comision = prompt("Ingrese el numero de comision")
+
+// console.log("Materia: " + materia + " Comision N°" + comision)
+
+
+let numeroA = parseInt(prompt("Ingrese el primer numero")) 
+let numeroB = parseInt(prompt("Ingrese el segundo numero")) 
+let resultado = numeroA + numeroB
+console.log(resultado)
